cc033efed4c39febf47f1ec6a15553447c32f86c
prathimasree08 <17wh1a05b8@bvrithyderabad.edu.in>
2023-04-26 16:51:02 +0530

    added routes,filters and status code(404)

--- express.js
+++ express.js
@@ -5,25 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="count"><button type="submit">Add Product</button></form>')
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
-
-app.use('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next) => {
-    res.send('<h1>Hello from Express!</h1>')
-    
-});
-
-
-
-
 
 
 
