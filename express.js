d57af94757e28afe07bd4b35c4736c6cdd7aed11
prathimasree08 <17wh1a05b8@bvrithyderabad.edu.in>
2023-04-26 12:24:25 +0530

    added body parser and input tag

--- express.js
+++ express.js
@@ -1,20 +1,30 @@
 //const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware inline
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="count"><button type="submit">Add Product</button></form>')
 });
 
-app.use((req,res,next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>')
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Hello from Express!</h1>')
+    
+});
+
+
+
+
+
+
 
 app.listen(3000);
-/*const server = http.createServer(app);
-
-server.listen(3000);*/
