e83560a4fa1a91911f3b25f44b814df555135955
prathimasree08 <17wh1a05b8@bvrithyderabad.edu.in>
2023-04-27 11:07:35 +0530

    Serving html,css and Js(views)

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,19 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.send('<h1>Hello from Express!</h1>')
-    
+    res.sendFile(path.join(rootDir,'views','shop.html'));
+});
+
+router.get('/contactus',(req,res,next) => {
+    res.sendFile(path.join(rootDir,'views','contactus.html'));
+});
+router.use('/success',(req,res,next) => {
+    res.sendFile(path.join(rootDir,'views','success.html'));
 });
 
 module.exports = router;
